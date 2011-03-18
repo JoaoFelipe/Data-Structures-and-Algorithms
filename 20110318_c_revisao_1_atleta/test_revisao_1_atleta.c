20290810b1b34668a4a1085cbb69a1fdecf1cc69
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 20:01:34 -0300

    Fri Mar 18 20:01:34 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -59,10 +59,10 @@ START_TEST("Remover extremos")
         ASSERT(vectors(resultado, retorno, 4) == 1);
     }
     
-    TEST("Remover extremos de 4.0, 1.0, 4.0, 4.0, 7.0, 7.0 deve retornar 1.0, 4.0, 4.0, 7.0");
+    TEST("Remover extremos de 4.0, 1.0, 4.0, 1.0, 7.0, 7.0 deve retornar 4.0, 4.0, 1.0, 7.0");
     {    
-        float notas[6] = {1.0, 1.0, 4.0, 4.0, 7.0, 7.0};
-        float retorno[4] = {1.0, 4.0, 4.0, 7.0};
+        float notas[6] = {4.0, 1.0, 4.0, 1.0, 7.0, 7.0};
+        float retorno[4] = {4.0, 4.0, 1.0, 7.0};
         float resultado[4] = {0};
         remove_extremos(notas, 6, resultado);
         ASSERT(vectors(resultado, retorno, 4) == 1);
