779defb7e0009a3f0a0501f90fba3b3e96cf2c92
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 15:13:54 -0300

    Fri Mar 18 15:13:54 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -6,7 +6,7 @@ START_TESTS()
 START_TEST("Testar Medias")
 
     TEST("Media de 1.0, 4.0, 4.0, 4.0, 4.0, 10.0 deve retornar 4.0");
-    ASSERT_EQUALS_FLOAT(media(1.0, 4.0, 4.0, 4.0, 4.0, 10.0), 4.0);
+    ASSERT_EQUALS_FLOAT(media([1.0, 4.0, 4.0, 4.0, 4.0, 10.0]), 4.0);
 //    ASSERT_EQUALS_FLOAT(1, 1);
 
 END_TEST()
