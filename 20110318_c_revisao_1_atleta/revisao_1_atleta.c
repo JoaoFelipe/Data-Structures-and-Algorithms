0e63890a9f5e809d41f867573e4625f7ac061508
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 16:12:41 -0300

    Fri Mar 18 16:12:41 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -1,8 +1,7 @@
 int media(float notas[], int quantidade)
 {
     int i;
-    (quantidade != NULL) ? quantidade = quantidade : quantidade = 6;
-    
+
     printf("%d\n",quantidade);
     return quantidade;
 }
