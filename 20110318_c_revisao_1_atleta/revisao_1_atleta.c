12e48f8354f11ccf2af3cebd72ba7e9f4c1d920e
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 16:14:41 -0300

    Fri Mar 18 16:14:41 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -1,8 +1,10 @@
-int media(float notas[], int quantidade)
+float media(float notas[], int quantidade)
 {
     int i;
-
-    printf("%d\n",quantidade);
-    return quantidade;
+    float resultado = 0;
+    for (i = 0; i<quantidade; i++){
+        resultado += notas[i]/6;
+    }
+    return resultado;
 }
 
