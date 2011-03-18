a8dc4d2009fd190d2598a163e3e4b0140a092f21
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 15:42:00 -0300

    Fri Mar 18 15:42:00 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -2,7 +2,7 @@ int media(float notas[])
 {
     int i;
     float retorno = 0;
-    for (i = 0; i< sizeof(notas); i++){
+    for (i = 0; i< sizeof(notas)/sizeof(notas[0]); i++){
         retorno += notas[i]/6;
     }
     return retorno;
