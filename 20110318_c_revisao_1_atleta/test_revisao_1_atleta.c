22922f6d0a0350630683481f091f25c254d32209
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 17:07:36 -0300

    Fri Mar 18 17:07:36 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -13,13 +13,14 @@ void print_vector(float vector[], int size)
 }
 int vectors(float vector1[], float vector2[], int size)
 {
+    int i;
     //print_vector(vector1, size);
     //print_vector(vector2, size);
     print_vector(vector1, size);
         print_vector(vector2, size);
 print_vector(vector1, size);
         print_vector(vector2, size);
-    for (int i = 0; i < size; i++){
+    for (i = 0; i < size; i++){
         print_vector(vector1, size);
         print_vector(vector2, size);
         if ((vector1[i] >= vector2[i] + 0.0005) || (vector1[i] <= vector2[i] - 0.0005)){ 
