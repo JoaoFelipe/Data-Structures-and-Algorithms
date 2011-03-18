c057ca8488c6a172b2f3d3fe80e3b4e5b7fa70a9
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 19:45:28 -0300

    Fri Mar 18 19:45:28 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -1,8 +1,7 @@
 float media(float notas[], int quantidade)
 {
-    int i;
     float resultado = 0;
-    for (i = 0; i<quantidade; i++){
+    for (int i = 0; i<quantidade; i++){
         resultado += notas[i]/6;
     }
     return resultado;
@@ -10,12 +9,7 @@ float media(float notas[], int quantidade)
 
 void remove_extremos(float notas[], int quantidade, float resultado[])
 {
-   
-    
     for (int i = 1; i<quantidade-1; i++){
         resultado[i-1] = notas[i];
     }
-//    return resultado;
-
-
 }
