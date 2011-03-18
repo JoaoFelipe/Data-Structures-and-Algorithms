74ffe0327c7c509d672f00f53c1b867e0ca94e9b
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 20:43:15 -0300

    Fri Mar 18 20:43:15 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -9,7 +9,7 @@ void remove_extremos(float notas[], int quantidade, float resultado[])
     }
 
     int k = 0;
-    for (int int i = 2; i<quantidade; i++){
+    for (int i = 2; i<quantidade; i++){
         if (notas[i] < min){
             resultado[k++] = min;
             min = notas[i];
