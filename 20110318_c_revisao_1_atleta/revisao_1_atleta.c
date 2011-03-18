efe5d4c8ebe711bdd74c00de5c4078227ba5fd8a
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 16:09:25 -0300

    Fri Mar 18 16:09:25 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -5,11 +5,7 @@ int media(float notas[], int quantidade)
     int tamanho = sizeof(notas);
     printf("%d\n",tamanho);
     return tamanho;
-    float retorno = 0;
 
-    for (i = 0; i< sizeof(notas)/sizeof(notas[0]); i++){
-        retorno += notas[i]/6;
-    }
     return retorno;
 }
 
