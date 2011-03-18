2d22e7ffb9c0fd9543339961350db9d2e471a1c7
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 16:13:12 -0300

    Fri Mar 18 16:13:12 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -8,19 +8,19 @@ START_TEST("Testar Medias")
     TEST("Media de 4.0, 4.0, 4.0, 4.0, 4.0, 4.0 deve retornar 4.0");
     {    
         float notas[6] = {4.0, 4.0, 4.0, 4.0, 4.0, 4.0};
-        ASSERT_EQUALS_FLOAT(media(notas, sizeof(notas)), 4.0);
+        ASSERT_EQUALS_FLOAT(media(notas, sizeof(notas)/sizeof(float)), 4.0);
     }
 
     TEST("Media de 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 deve retornar 5.0");
     {    
         float notas[6] = {5.0, 5.0, 5.0, 5.0, 5.0, 5.0};
-        ASSERT_EQUALS_FLOAT(media(notas, sizeof(notas)), 5.0);
+        ASSERT_EQUALS_FLOAT(media(notas, sizeof(notas)/sizeof(float)), 5.0);
     }
 
     TEST("Media de 1.0, 1.0, 4.0, 4.0, 7.0, 7.0 deve retornar 4.0");
     {    
         float notas[6] = {1.0, 1.0, 4.0, 4.0, 7.0, 7.0};
-        ASSERT_EQUALS_FLOAT(media(notas, sizeof(notas)), 4.0);
+        ASSERT_EQUALS_FLOAT(media(notas, sizeof(notas)/sizeof(float)), 4.0);
     }
 
 END_TEST()
