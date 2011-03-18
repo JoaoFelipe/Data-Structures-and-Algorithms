716f301d66329dc0cfcaa42844755f8f3fe1f258
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 15:46:30 -0300

    Fri Mar 18 15:46:30 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -1,7 +1,7 @@
 int media(float notas[])
 {
     int i;
-    int tamanho = sizeof(notas)/ sizeof(notas[0]);
+    int tamanho = sizeof(notas)/ sizeof(notas[1]);
     printf("%d", tamanho);
     return tamanho;
     float retorno = 0;
