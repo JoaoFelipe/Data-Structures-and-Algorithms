b3a5f5020d5b9448238abe16c87aaab0009aab9f
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 20:46:16 -0300

    Fri Mar 18 20:46:16 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -1,27 +1,6 @@
 #include "simplectest/tests.h"
 #include "revisao_1_atleta.c"
-
-void print_vector(float vector[], int size)
-{
-    printf("[");
-    for (int i = 0; i < size; i++){
-        printf("%f%s", vector[i], (i == size - 1) ? "": " ");
-    }
-    printf("]\n");
-
-}
-int vectors(float vector1[], float vector2[], int size)
-{
-    for (int i = 0; i < size; i++){
-        if ((vector1[i] >= (vector2[i] + 0.0005)) || (vector1[i] <= (vector2[i] - 0.0005))){ 
-            print_vector(vector1, size);
-            print_vector(vector2, size);
-            printf("%f != %f\n", vector1[i], vector2[i]);
-            return(0);
-        }
-    }
-    return(1);
-}
+#include "vector_test_functions.c"
 
 START_TESTS()
 
