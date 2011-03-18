cdab5900c90dedf68e081d287a78c1d0994db9ca
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 20:30:13 -0300

    Fri Mar 18 20:30:13 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -45,6 +45,12 @@ START_TEST("Testar Medias")
         ASSERT_EQUALS_FLOAT(media(notas, 6), 4.0);
     }
 
+    TEST("Media de 1.0, 2.0, 4.0, 4.0, 6.0, 9.0 deve retornar 4.0");
+    {    
+        float notas[6] = {1.0, 2.0, 4.0, 4.0, 6.0, 9.0};
+        ASSERT_EQUALS_FLOAT(media(notas, 6), 4.0);
+    }
+
 END_TEST()
 
 START_TEST("Remover extremos")
