4c1a8b9a96c5aa1c18334ab0ff7e50edd7562574
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-19 18:07:04 -0300

    Sat Mar 19 18:07:04 2011

--- 20110319_c_revisao_2_palavra_contida/test_revisao_2_palavra_contida.c
+++ 20110319_c_revisao_2_palavra_contida/test_revisao_2_palavra_contida.c
@@ -5,8 +5,11 @@ START_TESTS()
 
 START_TEST("Testar Palavra_contida")
 
-    TEST("'a' contido em 'b' deve retornar falso");
+    TEST("'a' contido em 'b' deve retornar 0");
     ASSERT(palavra_contida("a", "b") == 0);
+    
+    TEST("'a' contido em 'a' deve retornar 1");
+    ASSERT(palavra_contida("a", "a") == 1);
 //    ASSERT_EQUALS_FLOAT(1, 1);
 
 END_TEST()
