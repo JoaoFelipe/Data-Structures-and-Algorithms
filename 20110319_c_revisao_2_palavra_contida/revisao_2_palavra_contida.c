55534d6318dbf925afae6a32c3d651861207043d
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-19 18:36:29 -0300

    Sat Mar 19 18:36:29 2011

--- 20110319_c_revisao_2_palavra_contida/revisao_2_palavra_contida.c
+++ 20110319_c_revisao_2_palavra_contida/revisao_2_palavra_contida.c
@@ -1,8 +1,8 @@
 int palavra_contida(char *palavra, char *frase)
 {
     for (int i = 0; (*(frase + i) != '\0'); i++) {
-        if (*palavra == *(frase + i ) && (*++palavra == '\0'))
-            return 1;
+        if (*palavra == *(frase + i )) 
+            palavra++ ;
     } 
     return (*palavra == '\0' ? 1 : 0);
 }
