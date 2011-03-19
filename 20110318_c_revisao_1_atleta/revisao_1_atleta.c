9392363114ef094ceb5eead70df9dfc2c87357de
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-19 00:34:23 -0300

    Sat Mar 19 00:34:23 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -37,12 +37,12 @@ float media(float notas[], int quantidade)
     return media;
 }
 
-void vencedores(float** atletas, int no_atletas, int no_notas, int resultado[])
+void vencedores(float* atletas[], int no_atletas, int no_notas, int resultado[])
 {
     float medias[no_atletas];
     for (int i = 0; i < no_atletas; i++){
-        int* pos = *atletas + i*no_notas;
-        medias[i] = media(pos, no_notas);
+//        #float* pos = &atletas + i*no_notas;
+//        medias[i] = media(pos, no_notas);
         resultado[i] = i;
     }
 
