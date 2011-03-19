555bbf59bbef2ea513fd6cfa04d35bc9a2792923
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 21:03:45 -0300

    Fri Mar 18 21:03:45 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -56,7 +56,7 @@ END_TEST()
 
 START_TEST("Atletas")
     
-    TEST("Notas do Atleta 0: 1.0, 8.0, 7.0, 7.0, 10.0, 10.0 - Media 8.0"
+    TEST("Notas do Atleta 0: 1.0, 8.0, 7.0, 7.0, 10.0, 10.0 - Media 8.0"+
          "Notas do Atleta 1: 0.0, 0.0, 1.0, 0.0, 1.0, 1.0 - Media 0,5"+
          "Notas do Atleta 2: 10.0, 10.0, 10.0, 10.0, 10.0, 10.0 - Media 10.0"+
          "Deve retornar 2, 0, 1");
