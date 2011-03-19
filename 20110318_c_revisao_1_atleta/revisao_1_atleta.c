07b6cf34d3048a97dde6acb5a3f9b7360399692e
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-19 00:37:47 -0300

    Sat Mar 19 00:37:47 2011

--- 20110318_c_revisao_1_atleta/revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/revisao_1_atleta.c
@@ -41,7 +41,7 @@ void vencedores(float** atletas, int no_atletas, int no_notas, int resultado[])
 {
     float medias[no_atletas];
     for (int i = 0; i < no_atletas; i++){
-        * pos = &atletas + i*no_notas;
+        int* pos = &atletas + i*no_notas;
 //        medias[i] = media(pos, no_notas);
         resultado[i] = i;
     }
