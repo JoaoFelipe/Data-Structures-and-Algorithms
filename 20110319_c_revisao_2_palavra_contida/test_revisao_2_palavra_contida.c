d41e1b2ca4c9a616fbb8f3fca8d41a9417d8a17a
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-19 18:28:59 -0300

    Sat Mar 19 18:28:59 2011

--- 20110319_c_revisao_2_palavra_contida/test_revisao_2_palavra_contida.c
+++ 20110319_c_revisao_2_palavra_contida/test_revisao_2_palavra_contida.c
@@ -5,20 +5,20 @@ START_TESTS()
 
 START_TEST("Testar Palavra_contida")
 
-    TEST("'a' contido em 'b' deve retornar 0");
-    ASSERT(palavra_contida("a", "b") == 0);
-    
-    TEST("'a' contido em 'a' deve retornar 1");
-    ASSERT(palavra_contida("a", "a") == 1);
-    
-    TEST("'a' contido em 'ab' deve retornar 1");
-    ASSERT(palavra_contida("a", "ab") == 1);
-    
-    TEST("'a' contido em 'ba' deve retornar 1");
-    ASSERT(palavra_contida("a", "ba") == 1);
-    
-    TEST("'ac' contido em 'ba' deve retornar 0");
-    ASSERT(palavra_contida("ac", "ba") == 0);
+//    TEST("'a' contido em 'b' deve retornar 0");
+//    ASSERT(palavra_contida("a", "b") == 0);
+//    
+//    TEST("'a' contido em 'a' deve retornar 1");
+//    ASSERT(palavra_contida("a", "a") == 1);
+//    
+//    TEST("'a' contido em 'ab' deve retornar 1");
+//    ASSERT(palavra_contida("a", "ab") == 1);
+//    
+//    TEST("'a' contido em 'ba' deve retornar 1");
+//    ASSERT(palavra_contida("a", "ba") == 1);
+//    
+//    TEST("'ac' contido em 'ba' deve retornar 0");
+//    ASSERT(palavra_contida("ac", "ba") == 0);
     
     TEST("'ab' contido em 'ab' deve retornar 1");
     ASSERT(palavra_contida("ab", "ab") == 2);
