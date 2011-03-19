2f8cae94b73a46acbaae790ede33b4c4010551f4
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-18 21:29:33 -0300

    Fri Mar 18 21:29:33 2011

--- 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
+++ 20110318_c_revisao_1_atleta/test_revisao_1_atleta.c
@@ -54,21 +54,21 @@ START_TEST("Remover extremos")
 
 END_TEST()
 
-//START_TEST("Atletas")
+START_TEST("Atletas")
     
-//    TEST("Notas do Atleta 0: 1.0, 8.0, 7.0, 7.0, 10.0, 10.0 - Media 8.0; \
-//          Notas do Atleta 1: 0.0, 0.0, 1.0, 0.0, 1.0, 1.0 - Media 0,5; \
-//          Notas do Atleta 2: 10.0, 10.0, 10.0, 10.0, 10.0, 10.0 - Media 10.0; \
-//          Deve retornar 2, 0, 1");
-//    {
-//        float atletas[3][6] = {{1.0, 8.0, 7.0, 7.0, 10.0, 10.0}, {0.0, 0.0, 1.0, 0.0, 1.0, 1.0}, {10.0, 10.0, 10.0, 10.0, 10.0, 10.0}};
-//        int retorno[3] = {2, 0, 1};
-//        int resultado[3] = {0};
-//        svencedores(atletas, 3, 6, resultado);
-//        ASSERT(int_vectors(resultado, retorno, 3) == 1);
-//    }
+    TEST("Notas do Atleta 0: 1.0, 8.0, 7.0, 7.0, 10.0, 10.0 - Media 8.0; \
+          Notas do Atleta 1: 0.0, 0.0, 1.0, 0.0, 1.0, 1.0 - Media 0,5; \
+          Notas do Atleta 2: 10.0, 10.0, 10.0, 10.0, 10.0, 10.0 - Media 10.0; \
+          Deve retornar 2, 0, 1");
+    {
+        float atletas[3][6] = {{1.0, 8.0, 7.0, 7.0, 10.0, 10.0}, {0.0, 0.0, 1.0, 0.0, 1.0, 1.0}, {10.0, 10.0, 10.0, 10.0, 10.0, 10.0}};
+        int retorno[3] = {2, 0, 1};
+        int resultado[3] = {0};
+        svencedores(atletas, 3, 6, resultado);
+        ASSERT(int_vectors(resultado, retorno, 3) == 1);
+    }
 
-
+END_TEST()
 
 
 END_TESTS()
