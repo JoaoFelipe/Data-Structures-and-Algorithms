85da1e897c24312dfe80d382417388b5622d9608
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 02:40:59 -0300

    Sun Mar 20 02:40:59 2011

--- 20110320_c_revisao_5_tamanho_string/revisao_5_tamanho_string.c
+++ 20110320_c_revisao_5_tamanho_string/revisao_5_tamanho_string.c
@@ -7,7 +7,7 @@ int tamanho_string(char *string)
         return 1+tamanho_string(string+1);
 }
 
-int tamanhoString(char a[], int cont)
+int tamanhoString(char *a, int cont)
 {
     if (a[cont] != '\0')
         tamanhoString(a, cont++);
