0707ac45ecdfcad789137813aace12b8fe710c5f
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 15:38:58 -0300

    Sun Mar 20 15:38:58 2011

--- 20110320_c_revisao_10_vetores/revisao_10_vetores.c
+++ 20110320_c_revisao_10_vetores/revisao_10_vetores.c
@@ -1,14 +1,17 @@
-int soma(float *X, float *Y, int tamanho, float *retorno)
+void soma(float *X, float *Y, int tamanho, float *retorno)
 {
     *retorno = *X + *Y;
     if (tamanho > 0)
         soma(X+1, Y+1, tamanho -1, retorno+1);
 }
 
-int subtrair(float *X, float *Y, int tamanho, float *retorno)
+void subtrair(float *X, float *Y, int tamanho, float *retorno)
 {
     *retorno = *X - *Y;
     if (tamanho > 0)
         subtrair(X+1, Y+1, tamanho -1, retorno+1);
 }
 
+int procurar(int element, float *X, int tamanho){
+    return 0;
+}
