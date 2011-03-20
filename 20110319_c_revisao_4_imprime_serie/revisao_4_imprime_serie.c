84d6ac0698f546a7ef53c33f1f7f73636efa6887
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 02:14:33 -0300

    Sun Mar 20 02:14:33 2011

--- 20110319_c_revisao_4_imprime_serie/revisao_4_imprime_serie.c
+++ 20110319_c_revisao_4_imprime_serie/revisao_4_imprime_serie.c
@@ -1,11 +1,9 @@
 char* imprime_serie(int i, int j, int k, char *result)
 {
     sprintf(result, "%s%d", result, i);
-    if (i+k <= j){
+    if ((k > 0 && i+k <= j) || (k < 0 && i+k >= j)) ({
         sprintf(result, "%s ", result);
         imprime_serie(i+k, j, k, result);
     }
-    
-    return result;
 }
 
