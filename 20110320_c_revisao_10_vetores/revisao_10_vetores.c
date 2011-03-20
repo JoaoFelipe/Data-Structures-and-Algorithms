fc3ad79f90f3bf2690852b1e44abbf048857e7e7
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 15:32:30 -0300

    Sun Mar 20 15:32:30 2011

--- 20110320_c_revisao_10_vetores/revisao_10_vetores.c
+++ 20110320_c_revisao_10_vetores/revisao_10_vetores.c
@@ -9,6 +9,6 @@ int subtrair(float *X, float *Y, int tamanho, float *retorno)
 {
     *retorno = *X - *Y;
     if (tamanho > 0)
-        soma(X+1, Y+1, tamanho -1, retorno+1);
+        subtrair(X+1, Y+1, tamanho -1, retorno+1);
 }
 
