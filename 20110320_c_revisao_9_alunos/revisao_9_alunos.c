a17c20809d053517e2623233ec727f738e4451e5
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 13:46:21 -0300

    Sun Mar 20 13:46:21 2011

--- 20110320_c_revisao_9_alunos/revisao_9_alunos.c
+++ 20110320_c_revisao_9_alunos/revisao_9_alunos.c
@@ -6,7 +6,7 @@ float media(aluno a)
     return (a.nota1 + a.nota2 + a.nota3 + a.nota4) / 4;
 }
 
-void novo_aluno(aluno a, char *nome, float nota1, float nota2, float nota3, float nota4){
+void novo_aluno(aluno *a, char *nome, float nota1, float nota2, float nota3, float nota4){
     printf("%s: %f %f %f %f", a.nome, a.nota1, a.nota2, a.nota3, a.nota4);    
     sprintf(a.nome,"%s", nome);
     a.nota1 = nota1;
