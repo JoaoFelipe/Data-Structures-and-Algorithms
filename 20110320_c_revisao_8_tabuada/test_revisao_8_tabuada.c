5b8b538b1b61fbcc16cb509cdc210533a2532b8f
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 04:08:16 -0300

    Sun Mar 20 04:08:16 2011

--- 20110320_c_revisao_8_tabuada/test_revisao_8_tabuada.c
+++ 20110320_c_revisao_8_tabuada/test_revisao_8_tabuada.c
@@ -1,12 +1,13 @@
 #include "simplectest/tests.h"
 #include "revisao_8_tabuada.c"
+#include "vector_test_functions"
 
 START_TESTS()
 
 START_TEST("Testar Revisao_8_tabuada")
 
     TEST("Revisao_8_tabuada deve retornar 1");
-    ASSERT(revisao_8_tabuada() == 1);
+    ASSERT(tabuada() == 1);
 //    ASSERT_EQUALS_FLOAT(1, 1);
 
 END_TEST()
