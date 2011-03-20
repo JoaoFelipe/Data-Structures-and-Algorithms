029a67eceeabfc3b0afa67e26fc193476c7b6d2d
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 16:07:23 -0300

    Sun Mar 20 16:07:23 2011

--- 20110320_c_revisao_10_vetores/test_revisao_10_vetores.c
+++ 20110320_c_revisao_10_vetores/test_revisao_10_vetores.c
@@ -147,6 +147,14 @@ START_TEST("Procurar todos Elementos")
         ASSERT(strcmp(retorno, "1") == 0);
     }
 
+    TEST("Procurar todos elementos 1 em {2, 1, 1} deve retornar '1 2'");
+    {
+        float vet[] = {2, 1, 1};
+        char retorno[50] = "";
+        procurar_todos(1, vet, 3, retorno);
+        ASSERT(strcmp(retorno, "1 2") == 0);
+    }
+
 END_TEST()
 
 
