3076077ef24e156384aab70fc7efebb7a3345163
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 03:21:14 -0300

    Sun Mar 20 03:21:14 2011

--- 20110320_c_revisao_6_divisao/test_revisao_6_divisao.c
+++ 20110320_c_revisao_6_divisao/test_revisao_6_divisao.c
@@ -23,6 +23,9 @@ START_TEST("Testar divisao")
     TEST("5 dividido por 2 retorna 2");
     ASSERT(divisao(5, 2) == 2);
 
+    TEST("6 dividido por 2 retorna 3");
+    ASSERT(divisao(6, 2) == 3);
+
 END_TEST()
 
 
