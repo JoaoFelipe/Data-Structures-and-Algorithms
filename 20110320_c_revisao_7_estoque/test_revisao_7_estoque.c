4fe7eab1cc2896b927025591814324a38c1e92eb
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 03:30:26 -0300

    Sun Mar 20 03:30:26 2011

--- 20110320_c_revisao_7_estoque/test_revisao_7_estoque.c
+++ 20110320_c_revisao_7_estoque/test_revisao_7_estoque.c
@@ -7,7 +7,7 @@ START_TEST("Testar Media")
 
     TEST("Media do estoque 1,2,3,4,5,6,7 de uma semana deve retornar 7");
     {
-        int estoque[7] = { 1, 2, 3, 4, 5, 6, 7 }
+        int estoque[7] = { 1, 2, 3, 4, 5, 6, 7 };
         ASSERT_EQUALS_FLOAT(media(estoque, 7), 4.0);
     }
 //    ASSERT_EQUALS_FLOAT(1, 1);
