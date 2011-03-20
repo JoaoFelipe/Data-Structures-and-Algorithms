d6c1639ceaacb913f17223566aa093aea77c1913
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 02:28:58 -0300

    Sun Mar 20 02:28:58 2011

--- 20110319_c_revisao_4_imprime_serie/revisao_4_imprime_serie.c
+++ 20110319_c_revisao_4_imprime_serie/revisao_4_imprime_serie.c
@@ -12,7 +12,7 @@ char* imprime_serie_real(int i, int j, int k)
     printf("%d", i);
     if ((k > 0 && i+k <= j) || (k < 0 && i+k >= j)) {
         printf(" ");
-        imprime_serie(i+k, j, k, result);
+        imprime_serie(i+k, j, k);
     }
 }
 
