cb1550d158e914a36c02470014ea355f615e1d26
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 14:23:01 -0300

    Sun Mar 20 14:23:01 2011

--- 20110320_c_revisao_9_alunos/test_revisao_9_alunos.c
+++ 20110320_c_revisao_9_alunos/test_revisao_9_alunos.c
@@ -7,22 +7,22 @@ START_TEST("Testar Media Aluno")
 
     TEST("Media de Ana com notas 8.0,8.0,8.0 e 8.0 deve retornar 8,0");
     aluno ana;
-    novo_aluno(&ana, "Ana", 8.0, 8.0, 8.0, 8.0);
+    novo_aluno(&ana, "Ana", 8.0, 8.0, 8.0, 8.0, 2010);
     ASSERT_EQUALS_FLOAT(media(ana), 8.0);
 
     TEST("Media de Bia com notas 7.0,7.0,7.0 e 7.0 deve retornar 7,0");
     aluno bia;
-    novo_aluno(&bia, "Bia", 7.0, 7.0, 7.0, 7.0);
+    novo_aluno(&bia, "Bia", 7.0, 7.0, 7.0, 7.0, 2009);
     ASSERT_EQUALS_FLOAT(media(bia), 7.0);
 
     TEST("Media de Clara com notas 7.0,8.0,8.0 e 7.0 deve retornar 7,5");
     aluno clara;
-    novo_aluno(&clara, "Clara", 7.0, 8.0, 8.0, 7.0);
+    novo_aluno(&clara, "Clara", 7.0, 8.0, 8.0, 7.0, 2008);
     ASSERT_EQUALS_FLOAT(media(clara), 7.5);
    
     TEST("Media de Debora com notas 7.0,8.0,9.0 e 10.0 deve retornar 8,5");
     aluno debora;
-    novo_aluno(&debora, "Debora", 7.0, 8.0, 9.0, 10.0);
+    novo_aluno(&debora, "Debora", 7.0, 8.0, 9.0, 10.0, 2010);
     ASSERT_EQUALS_FLOAT(media(debora), 8.5);
 
 END_TEST()
