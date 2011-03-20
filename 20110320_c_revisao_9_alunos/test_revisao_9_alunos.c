bb0f0de7026525875f293dba8f14625ee4df9064
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 13:49:02 -0300

    Sun Mar 20 13:49:02 2011

--- 20110320_c_revisao_9_alunos/test_revisao_9_alunos.c
+++ 20110320_c_revisao_9_alunos/test_revisao_9_alunos.c
@@ -12,29 +12,17 @@ START_TEST("Testar Media Aluno")
 
     TEST("Media de Bia com notas 7.0,7.0,7.0 e 7.0 deve retornar 7,0");
     aluno bia;
-    sprintf(bia.nome, "%s", "Bia");
-    bia.nota1 = 7.0;
-    bia.nota2 = 7.0;
-    bia.nota3 = 7.0;
-    bia.nota4 = 7.0;
+    novo_aluno(&bia, "Bia", 7.0, 7.0, 7.0, 7.0);
     ASSERT_EQUALS_FLOAT(media(bia), 7.0);
 
     TEST("Media de Clara com notas 7.0,8.0,8.0 e 7.0 deve retornar 7,5");
     aluno clara;
-    sprintf(clara.nome, "%s", "Clara");
-    clara.nota1 = 7.0;
-    clara.nota2 = 8.0;
-    clara.nota3 = 8.0;
-    clara.nota4 = 7.0;
+    novo_aluno(&clara, "Clara", 7.0, 8.0, 8.0, 7.0);
     ASSERT_EQUALS_FLOAT(media(clara), 7.5);
    
      TEST("Media de Debora com notas 7.0,8.0,9.0 e 10.0 deve retornar 8,5");
     aluno debora;
-    sprintf(debora.nome, "%s", "Debora");
-    debora.nota1 = 7.0;
-    debora.nota2 = 8.0;
-    debora.nota3 = 9.0;
-    debora.nota4 = 10.0;
+    novo_aluno(&debora, "Clara", 7.0, 8.0, 9.0, 10.0);
     ASSERT_EQUALS_FLOAT(media(debora), 8.5);
 
 END_TEST()
