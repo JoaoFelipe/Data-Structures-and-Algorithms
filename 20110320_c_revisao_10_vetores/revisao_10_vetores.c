bac473783b23a7fff7df600a6fcdcdf1d832d11f
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 15:25:50 -0300

    Sun Mar 20 15:25:50 2011

--- 20110320_c_revisao_10_vetores/revisao_10_vetores.c
+++ 20110320_c_revisao_10_vetores/revisao_10_vetores.c
@@ -1,5 +1,8 @@
 int soma(float *X, float *Y, int tamanho, float *retorno)
 {
-    *retorno = *X + *Y;
+    if (tamanho > 0)
+        *retorno = *X + *Y;
+    else
+        soma(X+1, Y+1, tamanho -1, retorno+1);
 }
 
