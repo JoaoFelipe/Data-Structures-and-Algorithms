b3ce5491f4e2de8509431f0bf4c200b19ecf2b81
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 15:42:18 -0300

    Sun Mar 20 15:42:18 2011

--- 20110320_c_revisao_10_vetores/revisao_10_vetores.c
+++ 20110320_c_revisao_10_vetores/revisao_10_vetores.c
@@ -12,6 +12,10 @@ void subtrair(float *X, float *Y, int tamanho, float *retorno)
         subtrair(X+1, Y+1, tamanho -1, retorno+1);
 }
 
-int procurar(int element, float *X, int tamanho){
-    return 0;
+int procurar(float elemento, float *X, int tamanho){
+    if (*X == elemento)
+        return 0;
+    if (tamanho == 0)
+        return -1;
+    return 1+procurar(elemento, X+1, tamanho -1);
 }
