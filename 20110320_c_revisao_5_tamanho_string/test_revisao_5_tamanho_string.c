49828b96c1630668f336cd893e314fbf035448dd
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 02:44:00 -0300

    Sun Mar 20 02:44:00 2011

--- 20110320_c_revisao_5_tamanho_string/test_revisao_5_tamanho_string.c
+++ 20110320_c_revisao_5_tamanho_string/test_revisao_5_tamanho_string.c
@@ -7,15 +7,12 @@ START_TEST("Testar Tamanho da String")
 
     TEST("Tamanho da string '' deve retornar 0");
     ASSERT(tamanho_string("") == 0);
-    ASSERT(tamanhoString("",0) == 0);
 
     TEST("Tamanho da string 'a' deve retornar 1");
     ASSERT(tamanho_string("a") == 1);
-    ASSERT(tamanhoString("a",0) == 1);
 
     TEST("Tamanho da string 'ab' deve retornar 2");
     ASSERT(tamanho_string("ab") == 2);
-    ASSERT(tamanhoString("ab",0) == 2);
 
 END_TEST()
 
