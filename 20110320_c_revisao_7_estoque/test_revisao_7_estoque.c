5a7e0f3707a7cc389fdd17997760c647b1a4169d
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 03:45:58 -0300

    Sun Mar 20 03:45:58 2011

--- 20110320_c_revisao_7_estoque/test_revisao_7_estoque.c
+++ 20110320_c_revisao_7_estoque/test_revisao_7_estoque.c
@@ -19,30 +19,34 @@ START_TEST("Testar Media")
 
 END_TEST()
 
-START_TEST("Testar Mínino")
+START_TEST("Testar Mínino e Maximo")
 
-    TEST("Minimo do estoque 1,2,3,4,5,6,7 de uma semana deve retornar 1");
+    TEST("Estoque: 1,2,3,4,5,6,7. Mínimo = 1; Maximo = 7");
     {
         int estoque[7] = { 1, 2, 3, 4, 5, 6, 7 };
         ASSERT(minimo(estoque, 7) == 1);
+        ASSERT(maximo(estoque, 7) == 7);
     }
 
-    TEST("Minimo do estoque 0,2,3,4,5,6,7 de uma semana deve retornar 0");
+    TEST("Estoque: 0,2,3,4,5,6,8. Mínimo = 0; Maximo = 8");
     {
-        int estoque[7] = { 0, 2, 3, 4, 5, 6, 7 };
+        int estoque[7] = { 0, 2, 3, 4, 5, 6, 8 };
         ASSERT(minimo(estoque, 7) == 0);
+        ASSERT(maximo(estoque, 7) == 8);
     }
 
-    TEST("Minimo do estoque 6,2,3,4,5,6,9 de uma semana deve retornar 2");
+    TEST("Estoque: 6,2,3,4,5,9,6. Mínimo = 2; Máximo = 9");
     {
-        int estoque[7] = { 6, 2, 3, 4, 5, 6, 9 };
+        int estoque[7] = { 6, 2, 3, 4, 5, 9, 6 };
         ASSERT(minimo(estoque, 7) == 2);
+        ASSERT(maximo(estoque, 7) == 9);
     }
 
-    TEST("Minimo do estoque 6,4,10,3,5,6,9 de uma semana deve retornar 3");
+    TEST("Estoque: 6,4,10,3,5,6,9. Mínimo = 3; Máximo = 10");
     {
         int estoque[7] = { 6, 4, 10, 3, 5, 6, 9 };
         ASSERT(minimo(estoque, 7) == 3);
+        ASSERT(maximo(estoque, 7) == 10);
     }
 
 END_TEST()
