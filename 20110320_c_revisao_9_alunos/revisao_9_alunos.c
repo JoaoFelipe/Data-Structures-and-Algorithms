739ca6362d812e76952936c60f98fa703dcc1683
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 13:00:40 -0300

    Sun Mar 20 13:00:40 2011

--- 20110320_c_revisao_9_alunos/revisao_9_alunos.c
+++ 20110320_c_revisao_9_alunos/revisao_9_alunos.c
@@ -1,10 +1,11 @@
-typedef struct aluno {
+struct Aluno {
     char nome[50];
     float nota1;
     float nota2;
     float nota3;
     float nota4;
 };
+typedef Aluno aluno;
 
 int media(aluno a)
 {
