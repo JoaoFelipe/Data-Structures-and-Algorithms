bd17b608b8853e6d857797064849f953021c44c1
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 01:28:57 -0300

    Sun Mar 20 01:28:56 2011

--- 20110319_c_revisao_4_imprime_serie/vector_test_functions.c
+++ 20110319_c_revisao_4_imprime_serie/vector_test_functions.c
@@ -46,10 +46,11 @@ int int_vectors(int vector1[], int vector2[], int size)
 
 int char_vectors(char *vector1, char *vector2)
 {
-    if (*vector1 != *vector2)
+    if (*vector1 != *vector2){
         return 0;
-    else if (*vector1 == '\0')
+    } else if (*vector1 == '\0'){
         return 1;
+    }
     return char_vectors(vector1 + 1, vector2 + 1);
 }
 
