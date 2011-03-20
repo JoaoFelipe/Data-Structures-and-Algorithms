56e10f51e81039eb9d8635b30dafec8b64e6ddf5
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 04:46:01 -0300

    Sun Mar 20 04:46:01 2011

--- 20110320_c_revisao_8_tabuada/test_revisao_8_tabuada.c
+++ 20110320_c_revisao_8_tabuada/test_revisao_8_tabuada.c
@@ -59,7 +59,7 @@ START_TEST("Testar Tabuada de Tabela")
         retorno = (int *) malloc(n*n*sizeof(int));
         tabuada_tabela(n, retorno);
         int resultado[3][3] = {{1,2,3},{2,4,6},{3,6,9}};
-        ASSERT(int_vectors(retorno, &resultado, n*n) == 1);
+        ASSERT(int_vectors(retorno, (int *)&resultado, n*n) == 1);
     }
     
     TEST("Tabuada de tabela de n=6 deve retornar {1,2,3,4,5,6},{2,4,6,8,10,12},{3,6,9,12,15,18},{4,8,12,16,20,24},{5,10,15,20,25,30},{6,12,18,24,30,36}");
@@ -69,7 +69,7 @@ START_TEST("Testar Tabuada de Tabela")
         retorno = (int *) malloc(n*n*sizeof(int));
         tabuada_tabela(n, retorno);
         int resultado[6][6] = {{1,2,3,4,5,6},{2,4,6,8,10,12},{3,6,9,12,15,18},{4,8,12,16,20,24},{5,10,15,20,25,30},{6,12,18,24,30,36}};
-        ASSERT(int_vectors(retorno, &resultado, n*n) == 1);
+        ASSERT(int_vectors(retorno, (int *)&resultado, n*n) == 1);
         free(retorno);
     }
 
