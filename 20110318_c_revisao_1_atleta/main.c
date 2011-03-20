7e0e044195d0fc6a6ee8d65a7521dc61d17cd045
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 20:14:27 -0300

    Interação com usuario, Revisao 1

--- 20110318_c_revisao_1_atleta/main.c
+++ 20110318_c_revisao_1_atleta/main.c
@@ -1,3 +1,57 @@
-int main(void){
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "revisao_1_atleta.c"
+gi
+void ler_numero_de_atletas(int *num)
+{
+    printf("Digite o numero de atletas\n");
+    scanf("%d", num);
 }
+
+void ler_notas_dos_atletas(float *atletas, int num)
+{
+    for (int i = 0; i < num; i++){
+        for (int j = 0; j< 6; j++) {
+            printf("Digite a nota %d\n", j+1);
+            scanf("%f", atletas+i*6 +j);
+        }
+    }
+}
+
+void imprimir_medias_dos_atletas(float *atletas, int num)
+{
+    printf("Medias:\n");
+    for (int i = 0; i < num; i++){
+        printf("Atleta %d: %f\n", i+1, media(atletas + 6*i, 6));
+    }
+}
+
+void imprimir_ordem_de_vencedores(float *atletas, int num)
+{
+    int *ordem = (int *) malloc(num*sizeof(int));
+    vencedores(atletas, num, 6, ordem);
+
+    printf("Vencedores:\n");
+    for (int i = 0; i < num; i++){
+        printf("%d-Atleta %d: %f\n", i+1, ordem[i]+1, media(atletas + 6*ordem[i], 6));
+    }
+
+    free(ordem);
+}
+
+
+int main(void)
+{
+    int num;
+    ler_numero_de_atletas(&num);
+    float *atletas = (float *) malloc(num*6*sizeof(float));
+    
+    ler_notas_dos_atletas(atletas, num);
+    printf("\n");
+    imprimir_medias_dos_atletas(atletas, num);
+    printf("\n");
+    imprimir_ordem_de_vencedores(atletas, num);
+
+    free(atletas);
+}
+
