864a911fc268ca12834c1a8d7f0f622d31ff7c21
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 04:30:34 -0300

    Sun Mar 20 04:30:34 2011

--- 20110320_c_revisao_8_tabuada/revisao_8_tabuada.c
+++ 20110320_c_revisao_8_tabuada/revisao_8_tabuada.c
@@ -5,3 +5,10 @@ void tabuada(int num, int quantidade, int *retorno)
     }
 }
 
+void tabuada_tabela(int n, int *retorno)
+{
+    for (int i = 0; i < n; i++){
+        tabuada(n, n, (retorno+(i*n)))
+    }
+}
+
