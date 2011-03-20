701e58e581d3797e6bb6f5ff9386acd9fbf9d183
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 04:38:53 -0300

    Sun Mar 20 04:38:53 2011

--- 20110320_c_revisao_8_tabuada/test_revisao_8_tabuada.c
+++ 20110320_c_revisao_8_tabuada/test_revisao_8_tabuada.c
@@ -1,10 +1,8 @@
 #include "simplectest/tests.h"
 #include "revisao_8_tabuada.c"
 #include "vector_test_functions.h"
-void *calloc(size_t nmemb, size_t size);
 void *malloc(size_t size);
 void free(void *ptr);
-void *realloc(void *ptr, size_t size)
 
 START_TESTS()
 
