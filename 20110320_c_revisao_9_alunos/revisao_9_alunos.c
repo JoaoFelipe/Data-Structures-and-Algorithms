0fc521252b568d00a26cd8c298daaf75e96fd1a1
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 13:46:56 -0300

    Sun Mar 20 13:46:56 2011

--- 20110320_c_revisao_9_alunos/revisao_9_alunos.c
+++ 20110320_c_revisao_9_alunos/revisao_9_alunos.c
@@ -7,12 +7,12 @@ float media(aluno a)
 }
 
 void novo_aluno(aluno *a, char *nome, float nota1, float nota2, float nota3, float nota4){
-    printf("%s: %f %f %f %f", a.nome, a.nota1, a.nota2, a.nota3, a.nota4);    
-    sprintf(a.nome,"%s", nome);
-    a.nota1 = nota1;
-    a.nota2 = nota2;
-    a.nota3 = nota3;
-    a.nota4 = nota4;
-    printf("%s: %f %f %f %f", a.nome, a.nota1, a.nota2, a.nota3, a.nota4);    
+    printf("%s: %f %f %f %f", a->nome, a->nota1, a->nota2, a->nota3, a->nota4);    
+    sprintf(a->nome,"%s", nome);
+    a->nota1 = nota1;
+    a->nota2 = nota2;
+    a->nota3 = nota3;
+    a->nota4 = nota4;
+    printf("%s: %f %f %f %f", a->nome, a->nota1, a->nota2, a->nota3, a->nota4);    
 }
 
