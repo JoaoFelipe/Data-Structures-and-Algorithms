2c44020a0329b6a1f3977d223bab2eec9c179b57
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 16:20:52 -0300

    Sun Mar 20 16:20:52 2011

--- 20110320_c_revisao_10_vetores/revisao_10_vetores.c
+++ 20110320_c_revisao_10_vetores/revisao_10_vetores.c
@@ -24,11 +24,12 @@ int procurar(float elemento, float *X, int tamanho){
 }
 
 void procurar_todos(float elemento, float *X, int tamanho, char *retorno){
+    int extra = 0;
     while (1) {
-        int pos = procurar(elemento, X, tamanho);
+        int pos = extra + procurar(elemento, X + extra, tamanho -extra);
         if (pos == -1)
             return ;
         sprintf(retorno, "%s%s%d",retorno, strcmp(retorno,"") == 0 ? "" : " ", pos);
-        X++; tamanho--;
+        extra++
     }
 }
