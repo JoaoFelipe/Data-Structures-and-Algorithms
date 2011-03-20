b8519bd27d91ed59a2747b4b36e0418a26cb029e
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 04:22:02 -0300

    Sun Mar 20 04:22:02 2011

--- 20110320_c_revisao_8_tabuada/revisao_8_tabuada.c
+++ 20110320_c_revisao_8_tabuada/revisao_8_tabuada.c
@@ -2,7 +2,6 @@ void tabuada(int num, int quantidade, int *retorno)
 {
     for (int i = 0; i < quantidade; i++){
         retorno[i] = num * (i + 1);
-        printf("%d, %d", i, retorno[i]);
     }
 }
 
