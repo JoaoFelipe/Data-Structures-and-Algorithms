8e0d682799c2b26ff4a3f2ae452b9b7df7a8444d
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 01:56:52 -0300

    Sun Mar 20 01:56:52 2011

--- 20110319_c_revisao_4_imprime_serie/test_revisao_4_imprime_serie.c
+++ 20110319_c_revisao_4_imprime_serie/test_revisao_4_imprime_serie.c
@@ -8,15 +8,15 @@ START_TEST("Testar Revisao_4_imprime_serie")
 
     TEST("Imprime Serie de 1 até 1 com passo 1 deve retornar '1'");
     {
-        char saida[80]="";
-        char resultado[80] = "1";
+        char* saida="";
+        char* resultado = "1";
         ASSERT(char_vectors(imprime_serie(1, 1, 1, &saida), &resultado) == 1);
     }
 
     TEST("Imprime Serie de 2 até 2 com passo 1 deve retornar '2'");
     {
-        char saida[80]="";
-        char resultado[80] = "2";
+        char* saida="";
+        char* resultado = "2";
         ASSERT(char_vectors(imprime_serie(2, 2, 1, &saida), &resultado) == 1);
     }
 
