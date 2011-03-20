96ff33445622b9198ad8f81eb0026a86443393ed
João Felipe Nicolaci Pimentel <joaofelipe@joao-notebook.(none)>
2011-03-20 02:08:02 -0300

    Sun Mar 20 02:08:02 2011

--- 20110319_c_revisao_4_imprime_serie/test_revisao_4_imprime_serie.c
+++ 20110319_c_revisao_4_imprime_serie/test_revisao_4_imprime_serie.c
@@ -11,14 +11,16 @@ START_TEST("Testar Revisao_4_imprime_serie")
     {
         char saida[80] = "";
         char resultado[80] = "1";
-        ASSERT(strcmp(imprime_serie(1, 1, 1, saida), resultado) == 0);
+        imprime_serie(1, 1, 1, saida);
+        ASSERT(strcmp(saida, resultado) == 0);
     }
 
     TEST("Imprime Serie de 2 até 2 com passo 1 deve retornar '2'");
     {
         char saida[80] = "";
         char resultado[80] = "2";
-        ASSERT(strcmp(imprime_serie(2, 2, 1, saida), resultado) == 0);
+        imprime_serie(2, 2, 1, saida);
+        ASSERT(strcmp(saida, resultado) == 0);
     }
 
     TEST("Imprime Serie de 2 até 3 com passo 1 deve retornar '2 3'");
@@ -26,7 +28,6 @@ START_TEST("Testar Revisao_4_imprime_serie")
         char saida[80] = "";
         char resultado[80] = "2 3";
         imprime_serie(2, 3, 1, saida);
-        printf("%s", saida);
         ASSERT(strcmp(saida, resultado) == 0);
     }
 
